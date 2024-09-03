e88eae09780e91e6666c26c887394b7153744161
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-09-03 18:20:35 +0300

    Rewrite chat using card component.

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -7,12 +7,17 @@ import {
   List,
   ListItem,
   Typography,
+  Card,
+  CardContent,
+  CardActions,
+  useTheme,
 } from "@mui/material";
 
 const Chat = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [messages, setMessages] = useState<string[]>([]);
   const [input, setInput] = useState("");
+  const { palette } = useTheme();
 
   useEffect(() => {
     const newSocket = io("http://localhost:5173");
@@ -36,35 +41,59 @@ const Chat = () => {
   };
 
   return (
-    <Box sx={{ width: 300, p: 2, border: "1px solid grey", borderRadius: 2 }}>
-      <Typography variant="h6" gutterBottom>
-        Chat Room
-      </Typography>
-      <List sx={{ maxHeight: 200, overflowY: "auto" }}>
-        {messages.map((message, index) => (
-          <ListItem key={index}>{message}</ListItem>
-        ))}
-      </List>
-      <TextField
-        fullWidth
-        label="Type a message"
-        variant="outlined"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") handleSendMessage();
-        }}
-        sx={{ my: 1 }}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleSendMessage}
-        fullWidth
-      >
-        Send
-      </Button>
-    </Box>
+    <Card
+      sx={{
+        width: 300,
+        p: 2,
+        borderRadius: 2,
+        bgcolor: palette.background.alt,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <CardContent>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ color: palette.secondary[200] }}
+        >
+          Chat Room
+        </Typography>
+        <List
+          sx={{
+            maxHeight: 200,
+            overflowY: "auto",
+            color: palette.secondary.main,
+          }}
+        >
+          {messages.map((message, index) => (
+            <ListItem key={index}>{message}</ListItem>
+          ))}
+        </List>
+      </CardContent>
+      <CardActions sx={{ flexDirection: "column", alignItems: "center" }}>
+        <TextField
+          fullWidth
+          label="Type a message"
+          variant="outlined"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleSendMessage();
+          }}
+          sx={{ my: 1, color: palette.secondary[200] }}
+        />
+        <Button
+          variant="contained"
+          onClick={handleSendMessage}
+          fullWidth
+          sx={{ color: palette.secondary[200] }}
+        >
+          Send
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 

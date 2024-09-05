a12f3befd723c90ef741ef07ae3242104d4934c6
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-09-05 21:16:37 +0300

    Remove excess imports

--- client/src/lib/test/renderWithProviders.tsx
+++ client/src/lib/test/renderWithProviders.tsx
@@ -1,13 +1,20 @@
+import React from "react";
 import { render } from "@testing-library/react";
 import {
   withRouterAndQueryClient,
   WithRouterAndQueryClientProps,
 } from "./withProviders";
 
+// Обертка для рендеринга компонента с провайдерами
 const renderWithProviders = (
-  Component: React.ComponentType<WithRouterAndQueryClientProps>
+  Component: React.ComponentType<WithRouterAndQueryClientProps>,
+  props?: Omit<React.ComponentProps<typeof Component>, "children">
 ) => {
-  return render(withRouterAndQueryClient(Component));
+  const WrappedComponent = withRouterAndQueryClient((props) => (
+    <Component {...props} />
+  ));
+
+  // return render(<WrappedComponent {...props} />); TODO
 };
 
 export { renderWithProviders };
--- client/src/lib/test/withProviders.tsx
+++ client/src/lib/test/withProviders.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface WithRouterAndQueryClientProps {
   children: React.ReactNode;
 }
 
-// HOC для оборачивания компонентов в BrowserRouter и QueryClientProvider
+// HOC для оборачивания компонентов в QueryClientProvider и MemoryRouter
 const withRouterAndQueryClient = (
   Component: React.ComponentType<WithRouterAndQueryClientProps>
 ) => {
